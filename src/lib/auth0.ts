c54cf6bf39c29d585e9e920a4e9245b8e819831d
Eduardo Botelho <eduardo11224b@gmail.com>
2025-09-03 18:59:06 +0100

    fix: properly redirect back to homepage when authentication fails

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,3 +1,15 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
+import { NextResponse } from "next/server";
 
-export const auth0 = new Auth0Client();
+export const auth0 = new Auth0Client({
+    onCallback: async (err, ctx) => {
+        if (err)
+            return NextResponse.redirect(
+                new URL(`/?error=${err.message}`, process.env.APP_BASE_URL)
+            );
+
+        return NextResponse.redirect(
+            new URL(ctx.returnTo || "/", process.env.APP_BASE_URL)
+        );
+    }
+});
